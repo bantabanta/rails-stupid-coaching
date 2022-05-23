960127c7cbf8dd91146e68333aa69180c6bb0365
Tim Banta <timbanta@me.com>
2022-05-23 15:09:04 +0800

    Completed 'Stupid Coach' challenge with matched css styling

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# DUMB
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @answer = if params[:question].capitalize == 'I am going to work.'
+                'Great!'
+              elsif params[:question].end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
